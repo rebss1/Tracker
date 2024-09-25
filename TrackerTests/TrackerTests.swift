3220f8c5c6368a60e1495f889067c4b5424f0001
Loshilov Ilya <loshilovilya@yandex.ru>
2024-09-25 17:21:59 +0300

    Добавлено скриншот тестирование главное экрана

---
+++ TrackerTests/TrackerTests.swift
@@ -0,0 +1,23 @@
+//
+//  TrackerTests.swift
+//  TrackerTests
+//
+//  Created by Илья Лощилов on 25.09.2024.
+//
+
+import SnapshotTesting
+@testable import Tracker
+import XCTest
+
+final class TrackerTests: XCTestCase {
+
+    func testLightViewController() {
+        let vc = TrackerViewController()
+        assertSnapshots(matching: vc, as: [.image(traits: .init(userInterfaceStyle: .light))])
+    }
+    
+    func testDarkViewController() {
+        let vc = TrackerViewController()
+        assertSnapshots(matching: vc, as: [.image(traits: .init(userInterfaceStyle: .dark))])
+    }
+}
